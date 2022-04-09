26b8bd25a1981fdb8081d757c8a2c3f92c497044
mayk neves <mayksuel2727@gmail.com>
2022-04-09 20:01:35 -0300

    Commit Inicial

---
+++ src/main/java/com/github/mayksuel2727/testedevjunior/TesteDevJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.github.mayksuel2727.testedevjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDevJuniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteDevJuniorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mayksuel2727/testedevjunior/TesteDevJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mayksuel2727.testedevjunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDevJuniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
