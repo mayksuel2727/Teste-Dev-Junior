3e505f70a66a726c62207d91c0ed72947f8e1dc1
mayk neves <mayksuel2727@gmail.com>
2022-04-09 20:13:28 -0300

    feat: feito as Interfaces Repository TagRepository.java e ToolRepository.java

---
+++ src/main/java/com/github/mayksuel2727/testedevjunior/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.github.mayksuel2727.testedevjunior.repository;
+
+import com.github.mayksuel2727.testedevjunior.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+}
---
+++ src/main/java/com/github/mayksuel2727/testedevjunior/repository/ToolRepository.java
@@ -0,0 +1,7 @@
+package com.github.mayksuel2727.testedevjunior.repository;
+
+import com.github.mayksuel2727.testedevjunior.model.Tool;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToolRepository extends JpaRepository<Tool, Integer> {
+}
