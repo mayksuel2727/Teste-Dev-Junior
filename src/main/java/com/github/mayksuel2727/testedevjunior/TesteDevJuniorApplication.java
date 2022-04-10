756001fd385774af82598f2b02e4a553e8fb0a70
mayk neves <mayksuel2727@gmail.com>
2022-04-10 14:38:06 -0300

    feat: adicionada a anotação @EnableCaching para pode usar cache no projeto na classe main TesteDevJuniorApplication.java

--- src/main/java/com/github/mayksuel2727/testedevjunior/TesteDevJuniorApplication.java
+++ src/main/java/com/github/mayksuel2727/testedevjunior/TesteDevJuniorApplication.java
@@ -2,10 +2,12 @@ package com.github.mayksuel2727.testedevjunior;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 public class TesteDevJuniorApplication {
 
     public static void main(String[] args) {
