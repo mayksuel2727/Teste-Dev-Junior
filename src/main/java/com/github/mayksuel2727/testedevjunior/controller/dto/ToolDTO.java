918fa1115081a63e6086766173c8517ab6b05a18
mayk neves <mayksuel2727@gmail.com>
2022-04-10 00:13:40 -0300

    feat: criado a classe DTO ToolDTO.java

---
+++ src/main/java/com/github/mayksuel2727/testedevjunior/controller/dto/ToolDTO.java
@@ -0,0 +1,35 @@
+package com.github.mayksuel2727.testedevjunior.controller.dto;
+
+import com.github.mayksuel2727.testedevjunior.model.Tool;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.loader.collection.OneToManyJoinWalker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class ToolDTO {
+    private Integer id;
+    private String title;
+    private String link;
+    private String description;
+    private List<String> tag = new ArrayList<>();
+
+    public ToolDTO(Tool tool) {
+        this.id = tool.getId();
+        this.title = tool.getTitle();
+        this.link = tool.getLink();
+        this.description = tool.getDescription();
+        for (int i=0; i<tool.getTags().size(); i++) {
+            String descrip =null;
+            descrip = tool.getTags().get(i).getDescription();
+            this.tag.add(descrip);
+        }
+    }
+    public static List<ToolDTO> converter(List<Tool> tools) {
+        return tools.stream().map(ToolDTO::new).collect(Collectors.toList());
+    }
+}
