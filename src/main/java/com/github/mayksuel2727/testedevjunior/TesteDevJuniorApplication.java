a7d29b5c4983d5dd810cdb70c5bd9249e4bccfdc
mayk neves <mayksuel2727@gmail.com>
2022-04-10 13:26:29 -0300

    fix: adicionado a anotação @EnableSpringDataWebSupport na classe main

--- src/main/java/com/github/mayksuel2727/testedevjunior/TesteDevJuniorApplication.java
+++ src/main/java/com/github/mayksuel2727/testedevjunior/TesteDevJuniorApplication.java
@@ -2,8 +2,10 @@ package com.github.mayksuel2727.testedevjunior;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class TesteDevJuniorApplication {
 
     public static void main(String[] args) {
