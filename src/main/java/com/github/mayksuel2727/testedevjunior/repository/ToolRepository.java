c6dbfcba84f60d31883f1bc4683ce8eff435e197
mayk neves <mayksuel2727@gmail.com>
2022-04-10 13:32:01 -0300

    feat: criado busca no ToolRepository.java para fazer busca pelo Title

--- src/main/java/com/github/mayksuel2727/testedevjunior/repository/ToolRepository.java
+++ src/main/java/com/github/mayksuel2727/testedevjunior/repository/ToolRepository.java
@@ -1,7 +1,10 @@
 package com.github.mayksuel2727.testedevjunior.repository;
 
 import com.github.mayksuel2727.testedevjunior.model.Tool;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ToolRepository extends JpaRepository<Tool, Integer> {
+    Page<Tool> findByTitle(String title, Pageable pageable);
 }
